62e727d860d7f7e44447e43d2618a3ad0bbd4dc8
samkitshah1262 <samkitshah1262@gmail.com>
2022-11-06 19:42:57 +0530

    eventsponser

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -15,6 +15,8 @@ router.register("theme",ThemeViewSet)
 
 router.register("news",NewsViewSet)
 
+router.register("EventSponsors",EventSponsorsViewSet)
+
 urlpatterns=[
     path(r'',include(router.urls)),
  
