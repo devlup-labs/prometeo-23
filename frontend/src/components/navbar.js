ebd8715b1b1533d156f272c3305a98c4a266761b
YashBhargava02 <yasbh2002@gmail.com>
2022-11-19 01:21:37 +0530

    Fixed navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,80 +1,95 @@
 // import { useState, useEffect } from 'react';
-import { useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from 'react-router-dom'
 import './navbar.css'
 
 import PrometeoLogo from '../assets/navbar/prometeo_logo_23.png'
 
+const navBarLinks = [
+	{ name: 'theme', text: 'Theme', path: '/theme', section: 'left', tabIndex: 2 },
+	{ name: 'speakers', text: 'Past Speakers', path: '/past-speakers', section: 'left', tabIndex: 3 },
+	{ name: 'sponsors', text: 'Past Sponsors', path: '/past-sponsors', section: 'left', tabIndex: 4 },
+	{ name: 'events', text: 'Past Events', path: '/past-events', section: 'right', tabIndex: 5 },
+	{ name: 'gallery', text: 'Gallery', path: '/gallery', section: 'right', tabIndex: 6 },
+	{ name: 'register', text: 'Register', path: '/register', section: 'right', tabIndex: 7 },
+	{ name: 'login', text: 'Login', path: '/login', section: 'right', tabIndex: 8 },
+]
 
 function Navbar() {
-  const [showIcons, setShowIcons] = useState(false);
-    return (
-      <div id="navbar">
-        <nav
-          className={
-            showIcons
-              ? "mobile-left-menu-link"
-              : "navbar-left-section"
-          }
-        >
-          {/* speakers, sponsors, events */}
-          <Link to="/theme">
-            <div className="navbar-left-section-theme navbar-link">Theme</div>
-          </Link>
-          <Link to="/past-speakers">
-            <div className="navbar-left-section-speakers navbar-link">
-              Past Speakers
-            </div>
-          </Link>
-          <Link to="/past-sponsors">
-            <div className="navbar-left-section-sponsors navbar-link">
-              Past Sponsors
-            </div>
-          </Link>
-        </nav>
+	const navbarRef = useRef(null);
+	const [showIcons, setShowIcons] = useState(false);
 
-        <div className="navbar-logo">
-          <Link to="/">
-            <img id="navbar-logo-img" src={PrometeoLogo} alt="Prometeo Logo" />
-          </Link>
-        </div>
+	useEffect(() => {
+		function handleClickOutside(event) {
+			if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+				setShowIcons(false);
+			}
+		}
 
-        <nav
-          className={
-            showIcons
-              ? "mobile-right-menu-link"
-              : "navbar-right-section"
-          }
-        >
-          {/* gallery, theme, register, login */}
-          <Link to="/past-events">
-            <div className="navbar-right-section-events navbar-link">
-              Past Events
-            </div>
-          </Link>
-          <Link to="/gallery">
-            <div className="navbar-right-section-gallery navbar-link">
-              Gallery
-            </div>
-          </Link>
-          <Link to="/register">
-            <div className="navbar-right-section-register navbar-link">
-              Register
-            </div>
-          </Link>
-          <Link to="/login">
-            <div className="navbar-right-section-login">Login</div>
-          </Link>
-        </nav>
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [navbarRef, showIcons, setShowIcons]);
 
-        <div className="hamburger-menu">
-          <a href="#" onClick={() => setShowIcons(!showIcons)}>
-            <GiHamburgerMenu />
-          </a>
-        </div>
-      </div>
-    );
+	return (
+		<div id="navbar" ref={navbarRef}>
+			<nav className="navbar-left-section">
+				{/* speakers, sponsors, events */}
+				{
+					navBarLinks.filter(link => link.section === 'left').map(link => (
+						<Link to={link.path} key={link.name} >
+							<div className={`navbar-left-section-${link.name} navbar-link`} tabIndex={link.tabIndex}>
+								{link.text}
+							</div>
+						</Link>
+					))
+				}
+			</nav>
+
+			<div className="navbar-logo">
+				<Link to="/" tabIndex={1}>
+					<img id="navbar-logo-img" src={PrometeoLogo} alt="Prometeo Logo" onClick={() => {
+						if (showIcons) {
+							setShowIcons(false);
+						}
+					}} />
+				</Link>
+			</div>
+
+			<nav className="navbar-right-section">
+				{/* gallery, theme, register, login */}
+				{
+					navBarLinks.filter(link => link.section === 'right').map(link => (
+						<Link to={link.path} key={link.name} >
+							<div className={`navbar-right-section-${link.name} navbar-link`} tabIndex={link.tabIndex}>
+								{link.text}
+							</div>
+						</Link>
+					))
+				}
+			</nav>
+
+			<div className="hamburger-menu">
+				<GiHamburgerMenu className="hamburger-menu-svg" onClick={() => setShowIcons(!showIcons)} />
+			</div>
+
+			<div className="navbar-mobile">
+				{
+					showIcons
+					&&
+					navBarLinks.map(link => (
+						<Link to={link.path} key={link.name} onClick={() => setShowIcons(false)}>
+							<div className={`navbar-mobile-link navbar-link`}>
+								{link.text}
+							</div>
+						</Link>
+					))
+				}
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
