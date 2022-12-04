07274c4eaa31260473f4dee2cc746f3f36dff705
Yuvraj Rathva <rathva.1@iitj.ac.in>
2022-12-04 16:13:27 +0530

    initial change event card

--- frontend/src/pages/Event.js
+++ frontend/src/pages/Event.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import event_data from "./event_info";
-import './event_page.css';
+import "./event_page.css";
 
-import logo from '../assets/navbar/prometeo_logo_23.png';
-import FadeIn from '../components/fadein';
+import logo from "../assets/navbar/prometeo_logo_23.png";
+import FadeIn from "../components/fadein";
 
 // function logo(url){
 //    return(
@@ -12,58 +12,61 @@ import FadeIn from '../components/fadein';
 // }
 
 function createEntry(eventTerm) {
-	return (
-		<FadeIn duration={500}>
-			<Entry
-				key={eventTerm.id}
-				name={eventTerm.name}
-				img={eventTerm.image}
-				desc={eventTerm.description}
-			/>
-		</FadeIn>
-	);
+  return (
+    // <FadeIn duration={500}>
+      <Entry
+        key={eventTerm.id}
+        name={eventTerm.name}
+        img={eventTerm.image}
+        prize={eventTerm.prize}
+		date={eventTerm.date}
+      />
+    // </FadeIn>
+  );
 }
 
 function Entry(props) {
-	// console.log(props)
-	return (
-		<div className="event_cards">
-			<li className="card" aria-labelledby="event card">
-				<div className="card__filter">
-					<img className="card__photo" src={props.img} alt=" The image will be displayed here shortly " />
-				</div>
-				<div className="card__container">
-					<h2>{props.name}</h2>
-					{/* <time>Friday, July 10 • 7:00PM</time> */}
-					<p>{props.desc}</p>
-					{/* <a className="card__location" href="https://goo.gl/maps/dsPC54CdmnE2">Coral Sky Amphitheatre</a> */}
-					<div className="card__buttons">
-						<a href="#" className="card__buttons btn secondary" role="button" aria-haspopup="false">
-							View More
-							<i className="fas fa-arrow-right"></i>
-						</a>
-					</div>
-				</div>
-			</li>
-		</div>
-	);
+  // console.log(props)
+  return (
+    <a className="event_Card" href="#">
+      <div
+        className="event_Card-background"
+        style={{
+          backgroundImage: `url(${props.img})`,
+        }}
+      >
+        <div class="green_circle"></div>
+      </div>
+      <div className="event_Card-content">
+        {/* <div className="card"></div> */}
+        <h3 className="event_Card-heading">{props.name}</h3>
+        <h1 className="event_Card-date">{props.date}</h1>
+        <h3 className="event_Card-prize">Prize {props.prize}</h3>
+      </div>
+      <button className="button1">Register</button>
+      <button className="button2">View More</button>
+    </a>
+  );
 }
 
 function Events() {
-	useEffect(() => {
-		const navBarEle = document.getElementById("navbar")
-		navBarEle.style.opacity = 1;
-		document.body.style.overflow = "auto";
-	});
+  useEffect(() => {
+    const navBarEle = document.getElementById("navbar");
+    navBarEle.style.opacity = 1;
+    document.body.style.overflow = "auto";
+  });
 
-	return (
-		<div>
-			<div id="eventsPage">
-				<h2 className="section-header">PAST EVENTS</h2>
-				<div className="event_cards">{event_data.map(createEntry)}</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div id="eventsPage">
+        <h2 className="section-header">PAST EVENTS</h2>
+        <section className="event_Hero-section">
+          <div className="event_Card-grid">{event_data.map(createEntry)}</div>
+        </section>
+        {/* <div className="event_cards">{event_data.map(createEntry)}</div> */}
+      </div>
+    </div>
+  );
 }
 
 export default Events;
