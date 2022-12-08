ea6a606a463e1432477a599d4cf87b48802c54a6
Rohit <rohitkori897@gmail.com>
2022-12-08 23:10:07 +0530

    changing data to postgresql

--- backend/prometeo/settings.py
+++ backend/prometeo/settings.py
@@ -130,9 +130,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': config('prometeodb'),
-            'USER': config('prometeouser'),
-            'PASSWORD': config(''),
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
             'HOST': config('DB_HOST', default='localhost'),
             'PORT': config('DB_PORT', cast=int, default=5432),
         }
