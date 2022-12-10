cecfd0ef3064b8c2cf2544d8ac2a657200d689c4
Pranav1202 <94758297+pranav120802@users.noreply.github.com>
2022-12-11 00:18:42 +0530

    Added team-page and login page

---
+++ frontend/src/pages/team_info.js
@@ -0,0 +1,19 @@
+const team_data = [
+    {
+      id: 1,
+      image: "https://tovanidesign.com/wp-content/uploads/2019/02/C-_Users_Chrystina_Dropbox_tovani-design_size-examples-of-portraits_for-web_1-1-aspect-ratio-prints-at-square-for-social-media-for-web-photo-by-chrysti-tovani.jpg",
+      name: "Person 1",
+      por:"Front End Head ",
+    },
+    {
+        id: 2,
+        image:"https://i.ibb.co/G2CBWvb/person1.jpg",
+        name: "Person 2",
+        por:"Front End Head ",
+      },
+
+
+
+]
+
+export default team_data;
