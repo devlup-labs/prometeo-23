8b24c847b516e89a563c5a749ef205c0ec43a310
Soumil Sinha <sinha.5@iitj.ac.in>
2022-12-12 01:46:42 +0530

    responsiveness

--- frontend/src/components/initiativeHome.js
+++ frontend/src/components/initiativeHome.js
@@ -3,53 +3,35 @@ import { Link } from "react-router-dom";
 
 import "./initiativeHome.css";
 
-import initiativePageImg from "../assets/homePage/computer.png";
+import initiativePageImg from "../assets/homePage/umang.png";
 
 export default function InitiativeHome() {
     return (
         <div id="initiativeHome-container">
             <div id="initiativeHome">
-                <div id="initiativeHome-left-section">
-                    {/* <div id="initiativeHome-left-section-top"> */}
-                        <div id="initiativeHome-title">
-                            <div id="initiativeHome-subheading">
-                                SOCIAL INITIATIVE
-                            </div>
-                            <div id="initiativeHome-heading">
-                                UMANG
-                            </div>
-                        </div>
-                        <div id="initiativeHome-description">
-                            {/* lorem 100 */}
-                            Prometeo's vision is to raise awareness in society
-                            through social upliftment activities. We believe
-                            that it is the societal responsibility of the
-                            forefront of the nation to balance and empower those
-                            who are less fortunate. In post, we organized blood
-                            donation drives and year-long problem statements
-                            related to sustainability with big prize money,
-                            Continuing this legacy, In this edition of Prometeo,
-                            we aim to acquaint underprivileged women to new
-                            technologies and raise awareness about the heights
-                            they can achieve with the help of it. We plan to
-                            collaborate with several NGOs ond hold workshops for
-                            indigent children which will instigate their
-                            interest towards several domains of technology and
-                            inspire to pursue them.
-                        </div>
-                    {/* </div> */}
-                    {/* <div id="initiativeHome-left-section-links">
-                        <Link to="/initiative">
-                            <button
-                                id="preregister-button"
-                                className="button-29"
-                            >
-                                View More
-                            </button>
-                        </Link>
-                    </div> */}
+                <div id="initiativeHome-title">
+                    <div id="initiativeHome-subheading">SOCIAL INITIATIVE</div>
+                    <div id="initiativeHome-heading">UMANG</div>
                 </div>
-                <div id="initiativeHome-right-section">
+                <div id="initiativeHome-bottom-section">
+                    <div id="initiativeHome-description">
+                        {/* lorem 100 */}
+                        Prometeo's vision is to raise awareness in society
+                        through social upliftment activities. We believe that it
+                        is the societal responsibility of the forefront of the
+                        nation to balance and empower those who are less
+                        fortunate. In post, we organized blood donation drives
+                        and year-long problem statements related to
+                        sustainability with big prize money, Continuing this
+                        legacy, In this edition of Prometeo, we aim to acquaint
+                        underprivileged women to new technologies and raise
+                        awareness about the heights they can achieve with the
+                        help of it. We plan to collaborate with several NGOs ond
+                        hold workshops for indigent children which will
+                        instigate their interest towards several domains of
+                        technology and inspire to pursue them.
+                    </div>
+
                     <div id="initiativeHome-image">
                         <img
                             id="initiativeHome-image-img"
@@ -61,4 +43,20 @@ export default function InitiativeHome() {
             </div>
         </div>
     );
+}
+
+{
+    /* </div> */
+}
+{
+    /* <div id="initiativeHome-left-section-links">
+                        <Link to="/initiative">
+                            <button
+                                id="preregister-button"
+                                className="button-29"
+                            >
+                                View More
+                            </button>
+                        </Link>
+                    </div> */
 }
