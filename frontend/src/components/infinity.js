5ec5a4ff29e0f52b31ee6b39b3c7711e0243e67b
Soumil Sinha <sinha.5@iitj.ac.in>
2022-12-12 23:09:30 +0530

    infinity page glitch

--- frontend/src/components/infinity.js
+++ frontend/src/components/infinity.js
@@ -10,11 +10,10 @@ export default function Infinity() {
         <div id="infinity-container">
             <div id="infinity">
                 <img src={infinityImg} alt="Infinity" id="infinity-img" />
-                <span id="infinity-text">
-                    <h2>
-                        WHAT'S <span id="infinity-yellow">NEXT?</span>
-                    </h2>
-                </span>
+                <div id="infinity-text">
+                    <span>WHAT'S </span>
+                    <span id="infinity-yellow">NEXT?</span>
+                </div>
             </div>
             <div id="infinity-blur"></div>
         </div>
