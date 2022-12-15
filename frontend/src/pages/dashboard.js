3372d46cb3aabdd132fc78f6151212902708638e
YashBhargava02 <yasbh2002@gmail.com>
2022-12-15 19:19:01 +0530

    Update dashboard

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
+import { toast } from "react-toastify";
 
 import './dashboard.css';
 
@@ -64,7 +65,7 @@ const registered_events = [
 function Dashboard() {
     const [userData, setUserData] = useState({})
     const { user, logoutUser } = useContext(AuthContext);
-    console.log(user)
+    // console.log(user)
     const api = useAxios();
 
     useEffect(() => {
@@ -114,12 +115,43 @@ function Dashboard() {
                         </div>
                         <div id="dashboard-personalDetails-content">
                             <div className="dashboard-personalDetails-content-title">
-                                Name: <span className="dashboard-personalDetails-content-value">{user.username}</span>
+                                Name: <span className="dashboard-personalDetails-content-value">{user.username.replace('_', ' ')}</span>
                             </div>
                             <div className="dashboard-personalDetails-content-title">
                                 Email: <span className="dashboard-personalDetails-content-value">{user.email}</span>
                             </div>
+                            {
+                                userData.contact && (
+                                    <div className="dashboard-personalDetails-content-title">
+                                        Phone: <span className="dashboard-personalDetails-content-value">{userData.contact}</span>
+                                    </div>
+                                )
+                            }
                         </div>
+                        {
+                            userData.ambassador && (
+                                <div id="dashboard-caDetails">
+                                    <div id="dashboard-caDetails-title">
+                                        Campus Ambassdor Status
+                                    </div>
+                                    <div id="dashboard-caDetails-content">
+                                        <div className="dashboard-caDetails-content-title">
+                                            Invite code: <span className="dashboard-caDetails-content-value">{userData.invite_referral}</span>
+                                            <svg className="fa fa-copy" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+                                                onClick={() => {
+                                                    navigator.clipboard.writeText(userData.invite_referral)
+                                                    toast.info("Copied to clipboard", {
+                                                        position: "bottom-right",
+                                                    })
+                                                }}
+                                            >
+                                                <path d="M502.6 70.63l-61.25-61.25C435.4 3.371 427.2 0 418.7 0H255.1c-35.35 0-64 28.66-64 64l.0195 256C192 355.4 220.7 384 256 384h192c35.2 0 64-28.8 64-64V93.25C512 84.77 508.6 76.63 502.6 70.63zM464 320c0 8.836-7.164 16-16 16H255.1c-8.838 0-16-7.164-16-16L239.1 64.13c0-8.836 7.164-16 16-16h128L384 96c0 17.67 14.33 32 32 32h47.1V320zM272 448c0 8.836-7.164 16-16 16H63.1c-8.838 0-16-7.164-16-16L47.98 192.1c0-8.836 7.164-16 16-16H160V128H63.99c-35.35 0-64 28.65-64 64l.0098 256C.002 483.3 28.66 512 64 512h192c35.2 0 64-28.8 64-64v-32h-47.1L272 448z"/>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
                     </div>
                     <div id="dashboard-pass">
                         <div id="dashboard-pass-container">
