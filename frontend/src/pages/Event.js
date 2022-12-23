83fc2fd1838f3c03809f2f1318e501e0de0c4238
Soumil Sinha <sinha.5@iitj.ac.in>
2022-12-23 17:38:41 +0530

    fixed event details for now

--- frontend/src/pages/Event.js
+++ frontend/src/pages/Event.js
@@ -1,160 +1,292 @@
-import React, { useEffect, useState } from "react";
-import { useSearchParams, Link } from "react-router-dom";
-import event_data from "./event_info";
-import "./event.css";
+import React from "react";
+import { useState, useEffect } from "react";
+import { useLocation, useSearchParams, Link } from "react-router-dom";
 
-import logo from "../assets/navbar/prometeo_logo_23.png";
-import Footer from "../components/footer";
-import FadeIn from "../components/fadein";
+import "./EventDetails.css";
 import { backendURL } from "../backendURL";
+import FadeIn from "../components/fadein";
 
-// function logo(url){
-//    return(
-//     <img src={url} alt="sponsor_logo" />
-//    );
-// }
+function CreateEntry(props) {
+    const eventTerm = props.eventInfo;
+    const eventSponsor = props.eventSponsor;
 
-const eventTypeToName = {
-	"live": "Live Events",
-	"technical": "Technical Events",
-	"informal": "Informal Events",
-	"entrepreneurial": "Entrepreneurial Events",
-	"workshop": "Workshops",
-	"poster_presentation": "Poster Presentation",
-	"panel_discussion": "Panel Discussion",
-	"exhibition": "Tech Carnival",
+    return (
+        <Details
+            key={eventTerm.id}
+            eventTerm={eventTerm}
+            eventSponsor={eventSponsor}
+            // name={eventTerm.name}
+            // img={eventTerm.image.replace(
+            //     "0.0.0.0:8888",
+            //     "apiv.prometeo.in"
+            // )}
+            // desc={eventTerm.description}
+            // team_size={eventTerm.max_team_size}
+            // prize={eventTerm.prize}
+            // date={eventTerm.date}
+            // rulebook={eventTerm.rulebook}
+            // sponsor_name={eventSponsor.map((sponsor) => {
+            //     if (sponsor.name) return sponsor.name;
+            //     else return "#";
+            // })}
+            // sponsor_image={
+            //     // for each sponsor, we need to get the image from the backend
+            //     // and then display it here
+            //     eventSponsor.map((sponsor) => {
+            //         return sponsor.image.replace(
+            //             "0.0.0.0:8888",
+            //             "apiv.prometeo.in"
+            //         );
+            //     })
+            // }
+            // sponsor_website={eventSponsor.map((sponsor) => {
+            //     if (sponsor.website) return sponsor.website;
+            //     else return "#";
+            // })}
+        />
+    );
 }
 
-function createEntry(eventTerm) {
-	if (
-		eventTerm.name !== "Tedx"
-		&& eventTerm.type !== "talk" 
-	) {
-		return (
-			<Entry
-				key={eventTerm.id}
-				event={eventTerm}
-			/>
-		)
-	}
-	else return null;
+function Details(props) {
+    const eventTerm = props.eventTerm;
+    const eventSponsor = props.eventSponsor;
+
+    return (
+        <div className="event-details">
+            <div className="event-details__header">
+                <div className="event-details__title">
+                    {eventTerm.name}
+                    <div className="event-details__title__underline"></div>
+                </div>
+                {eventSponsor.length > 0 && (
+                    <div className="event-details__sponsors">
+                        <div className="event-details__sponsors_text">
+                            Sponsored by
+                        </div>
+                        <div className="event-details__sponsors_images">
+                            {eventSponsor.map((sponsor) => {
+                                return (
+                                    <img
+                                        src={
+                                            sponsor.image
+                                                ? sponsor.image.replace(
+                                                      "0.0.0.0:8888",
+                                                      "apiv.prometeo.in"
+                                                  )
+                                                : ""
+                                        }
+                                        title={sponsor.name}
+                                        alt="Sponsor Image"
+                                        key={sponsor.id}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            </div>
+            <div className="event-details__body">
+                <div className="event-details__body__left">
+                    {/* <div className="event-details__body__left__image"> */}
+                    <img
+                        className="event-details__body__left__image__img"
+                        src={
+                            eventTerm.image
+                                ? eventTerm.image.replace(
+                                      "0.0.0.0:8888",
+                                      "apiv.prometeo.in"
+                                  )
+                                : ""
+                        }
+                        alt="Event Image"
+                    />
+                    {/* </div> */}
+                    <div className="event-details__body__left__buttons">
+                        {eventTerm.external_link && (
+                            <a
+                                href={eventTerm.external_link || ""}
+                                className="event-details-register button-64"
+                            >
+                                <span>REGISTER</span>
+                            </a>
+                        )}
+                        {eventTerm.rulebook && (
+                            <a
+                                href={
+                                    eventTerm.rulebook.replace(
+                                        "0.0.0.0:8888",
+                                        "apiv.prometeo.in"
+                                    ) || ""
+                                }
+                                className="event-details-rulebook button-64"
+                            >
+                                <span>RULEBOOK</span>
+                            </a>
+                        )}
+                    </div>
+                </div>
+                <div className="event-details__body__right">
+                    <div className="event-details__body__right__top">
+                        <div className="event-details__body__right__top__on">
+                            On{" "}
+                            <span id="event-details-text">
+                                {eventTerm.date}
+                            </span>
+                        </div>
+                        <div className="event-details__body__right__top__prize">
+                            Prize Money{" "}
+                            <span id="event-details-text">
+                                {eventTerm.prize}
+                            </span>
+                        </div>
+                        <div className="event-details__body__right__top__participation-type">
+                            Participation Type{" "}
+                            <span id="event-details-text">
+                                {eventTerm.participation_type}
+                            </span>
+                        </div>
+                        {eventTerm.max_team_size > 1 && (
+                            <div className="event-details__body__right__top__teams">
+                                Team Size{" "}
+                                <span id="event-details-text">
+                                    {eventTerm.min_team_size}-
+                                    {eventTerm.max_team_size}
+                                </span>
+                            </div>
+                        )}
+                    </div>
+                    <hr className="event-details__body__right__bottom1__hr" />
+                    <div className="event-details__body__right__bottom">
+                        {eventTerm.description && (
+                            <>
+                                <p id="event-content-heading">Description</p>
+                                <div className="event-details__body__right__bottom2">
+                                    {eventTerm.description}
+                                </div>
+                                <hr className="event-details__body__right__bottom1__hr" />
+                            </>
+                        )}
+                        {eventTerm.problem_statement && (
+                            <>
+                                <p id="event-content-heading">
+                                    Problem Statement
+                                </p>
+                                <div
+                                    className="event-details__body__right__bottom1"
+                                    dangerouslySetInnerHTML={{
+                                        __html: eventTerm.problem_statement,
+                                    }}
+                                ></div>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-function Entry(props) {
-	const event = props.event;
-	// console.log("Event:", event)
-	return (
-		<div className="event_Card">
-			<div className="event_Card-background" 
-				style={{
-					backgroundImage: `url(${event.image.replace("0.0.0.0:8888", "apiv.prometeo.in")})`
-				}}
-			></div>
-			<div className="event_Card-content">
-				{/* <div className="card"></div> */}
-				<h3 className="event_Card-heading">{event.name}</h3>
+function EventDetails() {
+    const [eventSponsor, setEventSponsor] = useState([]);
+    const [eventInfo, setEventInfo] = useState([]);
+    const [urlParams] = useSearchParams();
 
-				<div className="event_Card-info">
-					<h1 className="event_Card-date">{event.date}</h1>
-					{
-						(event.prize && event.prize !== "NA") && 
-						<h3 className="event_Card-prize">Prize {event.prize}</h3>
-					}
-				</div>
-			</div>
-			<div id="buttons">
-				{/* <Link 
-					id="button1"
-					// to={{
-					// 	pathname: '/event-details/',
-					// 	search: `?id=${event.id}`,
-					// }}
-				>
-					Register
-				</Link> */}
-				{/* <button className="button2">View more</button> */}
-				<Link 
-					id="button2"
-					to={{
-						pathname: '/event-details/',
-						search: `?id=${event.id}&name=${event.name}`,
-					}}
-					state = {event}
-				>
-					View More
-				</Link>
-			</div>
-		</div>
-	);
+    const location = useLocation();
+    //   console.log(location.state);
+
+    useEffect(() => {
+        // console.log("useEffect");
+
+        async function fetchData() {
+            let headers = new Headers();
+            headers.append("Content-Type", "application/json");
+            headers.append("Accept", "application/json");
+            headers.append("Origin", "http://localhost:3000");
+
+            const requestOptions = {
+                method: "GET",
+                headers: headers,
+            };
+
+            const fetchURL = `${backendURL}/events/`;
+
+            await fetch(fetchURL, requestOptions)
+                .then((response) => response.json())
+                .then((data) => {
+                    data = data.filter(
+                        (item) => item.id == urlParams.get("id")
+                    );
+                    setEventInfo(data[0]);
+                    // console.log("Data:", data[0]);
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
+        }
+
+        const card = location.state;
+        // console.log("Card:", card);
+
+        if (card) {
+            //   console.log("pewpewpew");
+            setEventInfo(card);
+        } else {
+            // console.log("pewpewpew2");
+            fetchData();
+        }
+    }, []);
+
+    // useEffect(() => {
+    //   console.log("data fetched: ", eventInfo);
+    // }, [eventInfo]);
+
+    useEffect(() => {
+        async function fetchData2() {
+            let headers = new Headers();
+            headers.append("Content-Type", "application/json");
+            headers.append("Accept", "application/json");
+            headers.append("Origin", "http://localhost:3000");
+
+            const requestOptions = {
+                method: "GET",
+                headers: headers,
+            };
+
+            const fetchURL = `${backendURL}/eventsponsors/`;
+
+            await fetch(fetchURL, requestOptions)
+                .then((response) => response.json())
+                .then((data) => {
+                    data = data.filter(
+                        (item) => item.event == urlParams.get("id")
+                    );
+                    setEventSponsor([...data]);
+                    // console.log("Data:", data);
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
+        }
+
+        fetchData2();
+    }, []);
+
+    useEffect(() => {
+        const navBarEle = document.getElementById("navbar");
+        navBarEle.style.opacity = 1;
+        document.body.style.overflow = "auto";
+    });
+
+    return (
+        <FadeIn duration={500}>
+            <div id="EventDetailsPage" className="contentDiv">
+                <CreateEntry
+                    eventInfo={eventInfo}
+                    eventSponsor={eventSponsor}
+                />
+            </div>
+        </FadeIn>
+    );
 }
 
-function Events() {
-	const [urlParams] = useSearchParams();
-	// console.log("Type: ", urlParams.get("type"));
-
-	const [eventData, setEventData] = useState([])
-
-	useEffect(() => {
-		const navBarEle = document.getElementById("navbar");
-		navBarEle.style.opacity = 1;
-		document.body.style.overflow = "auto";
-	});
-
-	useEffect(() => {
-		async function fetchData() {
-			let headers = new Headers();
-			headers.append('Content-Type', 'application/json');
-			headers.append('Accept', 'application/json');
-			headers.append('Origin', 'http://localhost:3000');
-
-			const requestOptions = {
-				method: 'GET',
-				headers: headers,
-			}
-			
-			const fetchURL = (urlParams.get("type")) 
-				? `${backendURL}/events/?type=${urlParams.get("type")}` 
-				: `${backendURL}/events/`;
-			
-			await fetch(fetchURL, requestOptions)
-				.then((response) => response.json())
-				.then((data) => {
-					setEventData([
-						...data
-					]);
-					console.log("Data:", data);
-				})
-				.catch((error) => {
-					console.error("Error:", error);
-				});
-		}
-		fetchData();
-	}, [urlParams])
-
-	return (
-		<FadeIn duration={500}>
-			<div id="eventsPage" key={urlParams.get("type")}>
-				<h2 className="section-header">
-					{
-						urlParams.get("type") 
-						? eventTypeToName[urlParams.get("type")]
-						: "Events"
-					}
-				</h2>
-				<section className="event_Hero-section">
-					{
-						(eventData.length > 0) ?
-						<div className="event_Card-grid">{eventData.map(createEntry)}</div> :
-						<div className="event_Card-coming-soon">
-							Coming Soon!
-						</div>
-					}
-				</section>
-				{/* <div className="event_cards">{event_data.map(createEntry)}</div> */}
-				{/* <Footer /> */}
-			</div>
-		</FadeIn>
-	);
-}
-
-export default Events;
+export default EventDetails;
