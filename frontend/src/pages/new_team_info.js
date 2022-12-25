a3578ef70c536ffbf6e556547ceba40573e65f2b
Soumil Sinha <sinha.5@iitj.ac.in>
2022-12-25 21:02:13 +0530

    minor changes

--- frontend/src/pages/new_team_info.js
+++ frontend/src/pages/new_team_info.js
@@ -54,7 +54,7 @@ const team_data = [
         teamName: "Technical events",
         members: [
             {
-                id: 2,
+                id: 0,
                 image: "https://i.imgur.com/Z1gn8Wu.jpg",
                 name: "Adarsh Palaskar ",
                 por: "Head",
@@ -63,7 +63,7 @@ const team_data = [
                 linkedin: "https://www.linkedin.com/in/adarsh-palaskar/",
             },
             {
-                id: 0,
+                id: 1,
                 image: "https://i.imgur.com/AW0d3d5.jpg",
                 name: "Sumit Kumar Prajapati",
                 por: "Head",
@@ -72,6 +72,11 @@ const team_data = [
                 linkedin:
                     "https://www.linkedin.com/in/sumit-prajapati-2a0a551bb/",
             },
+        ],
+    },
+    {
+        teamName: "Tech Carnival",
+        members: [
             {
                 id: 1,
                 image: "https://i.imgur.com/ZjdLbgK.jpg",
