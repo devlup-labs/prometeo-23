50c06f91577f9b482551438ef5c055d10fe4ca24
samkitshah1262 <samkitshah1262@gmail.com>
2023-01-02 22:30:32 +0530

    sorry
    Co-authored-by: Soumil Sinha <79688127+sawmill811@users.noreply.github.com>
    Co-authored-by: Yash Bhargava <77163488+CyanTarantula@users.noreply.github.com>

--- backend/prometeo/static/dashboard/js/passtype.js
+++ backend/prometeo/static/dashboard/js/passtype.js
@@ -1,19 +1,19 @@
-function setPassType(email, passType) {
-    let data = { email: email, passType: passType };
+// function setPassType(email, passType) {
+//     let data = { email: email, passType: passType };
 
-    fetch("/dashboard/setpasstype/", {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    })
-    .then(res => {
-        console.log("Request complete! response:", res);
-        // window.location.reload();
-    }).then(data => {
-        // console.log("Request complete! data:", data);
-        window.location.href = "http://127.0.0.1:8000/dashboard/";
-    })
-    .catch(err => {
-        console.log("Request failed", err);
-    });
-}
+//     fetch("/dashboard/setpasstype/", {
+//         method: "POST",
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(data)
+//     })
+//     .then(res => {
+//         console.log("Request complete! response:", res);
+//         // window.location.reload();
+//     }).then(data => {
+//         // console.log("Request complete! data:", data);
+//         window.location.href = "http://127.0.0.1:8000/dashboard/";
+//     })
+//     .catch(err => {
+//         console.log("Request failed", err);
+//     });
+// }
