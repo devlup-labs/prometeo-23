8c2b8090e5b3f1d12eda804b989729eb96622a6a
Rohit <rohitkori897@gmail.com>
2023-01-10 18:26:26 +0530

    minor changes

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -815,10 +815,10 @@ def passtype_update(request):
             if "Jumbo Fee" in list_typeofpayment[i]:
                 # list_passtype[i]=3
                 list_passtype.append(3)
-            elif "Cultural Night Pass" in list_typeofpayment[i]:
+            elif "Event Fee" in list_typeofpayment[i]:
                 # list_passtype[i]=2
                 list_passtype.append(2)
-            elif "Accommodation Pass" in list_typeofpayment[i]:
+            elif "Accommodation Fee" in list_typeofpayment[i]:
                 # list_passtype[i]=1
                 list_passtype.append(1)
 
