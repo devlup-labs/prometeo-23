0297560ffdf3bc6d1a5fc0710665d9fc2de77404
Pranav1202 <94758297+pranav120802@users.noreply.github.com>
2023-01-12 03:00:02 +0530

    Added ticket on dashboard

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -9,10 +9,13 @@ import AuthContext from "../context/AuthContext";
 import useAxios from "../context/context_useAxios";
 
 import rocketImg from "../assets/icons/rocket.png";
-import PrometeoLogo from "../assets/homePage/prometeo-updated.png";
+// import PrometeoLogo from "../assets/homePage/prometeo-updated.png";
 import { Navigate } from "react-router-dom";
 import domtoimage from "dom-to-image";
-
+import PrometeoLogo from "../assets/homePage/prometeo-updated2.png";
+import Gmap from "../assets/homePage/maps.png";
+import passBg from "../assets/homePage/pass_bg2.jpg";
+import downIcon from "../assets/homePage/download_icon.png";
 import ticket1 from "../assets/ticket1.png";
 import ticket2 from "../assets/ticket2.png";
 import ticket3 from "../assets/ticket3.png";
@@ -35,10 +38,30 @@ function Dashboard() {
     const [userData, setUserData] = useState({});
     const [registeredEvents, setRegisteredEvents] = useState({});
     const { user, logoutUser } = useContext(AuthContext);
+
+    let maxNameLen = 10;
+    // if (user.username[0] === 'P') {
+    //     user.username = "Test " + user.username
+    // }
+    user.username.split('_').forEach((name) => {
+        name.split(' ').forEach((n) => {
+            maxNameLen = Math.max(maxNameLen, n.length)
+        })
+    })
+
+    // if (user.email[0] === 't') {
+    //     user.email = "abc " + user.email;
+    // }
+
+    let emailLength = 10;
+    emailLength = Math.max(emailLength, user.email.split('@')[0].length)
+    emailLength = Math.max(emailLength, user.email.split('@')[1].length + 1)
+    // console.log("MaxNameLen:", maxNameLen, " emailLength:", emailLength, " ", window.innerWidth)
+
     // console.log("User(dashboard):", user)
     const api = useAxios();
 
-	var tickets = [ticket1, ticket2, ticket3, ticket4]
+    var tickets = [ticket1, ticket2, ticket3, ticket4]
 
     useEffect(() => {
         const navBarEle = document.getElementById("navbar");
@@ -254,45 +277,45 @@ function Dashboard() {
         //         link.href = dataUrl;
         //         link.click();
         //     });
-		await load(tickets[ticket_num])
-			.then(() => {
-				domtoimage
-					.toBlob(document.getElementById("dashboard-download-ticket"))
-					.then(function (blob) {
-						var link = document.createElement("a");
-						link.download = userData.registration_id + "_ticket.png";
-						link.href = URL.createObjectURL(blob);
-						link.click();
-					});
-			})
-			.catch((err) => {
-				console.log(err);
-				throw(err)
-			})
+        await load(tickets[ticket_num])
+            .then(() => {
+                domtoimage
+                    .toBlob(document.getElementById("dashboard-download-ticket"))
+                    .then(function (blob) {
+                        var link = document.createElement("a");
+                        link.download = userData.registration_id + "_ticket.png";
+                        link.href = URL.createObjectURL(blob);
+                        link.click();
+                    });
+            })
+            .catch((err) => {
+                console.log(err);
+                throw (err)
+            })
     }
 
-	const handleDownload = (ticket_num) => {
-		const myPromise = new Promise((resolve, reject) => {
-		  downloadTicket(ticket_num)
-			.then((res) => {
-			  resolve(res);
-			})
-			.catch((err) => {
-			  reject(err);
-			})
-		})
-	
-		toast.promise(myPromise, {
-		  pending: "Starting download...",
-		  success: "Download started!",
-		  error: "Error downloading ticket"
-		})
-	
-	  }
+    const handleDownload = (ticket_num) => {
+        const myPromise = new Promise((resolve, reject) => {
+            downloadTicket(ticket_num)
+                .then((res) => {
+                    resolve(res);
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+        })
+
+        toast.promise(myPromise, {
+            pending: "Starting download...",
+            success: "Download started!",
+            error: "Error downloading ticket"
+        })
+
+    }
 
     const [download, setDownload] = useState(false);
 
-    useEffect(() => {}, [download]);
+    useEffect(() => { }, [download]);
 
     return (
         <div id="dashboard-container">
@@ -372,7 +395,32 @@ function Dashboard() {
                         )}
                     </div>
                     <div id="dashboard-pass">
-                        <div
+                        {
+                            (userData && userData.pass_type !== 0) ? (
+                                <img id="dashboard-pass-download-btn" src={downIcon} alt="Download Button" onClick={() => handleDownload(userData.pass_type)} />
+                            ) : (
+                                <div id="dashboard-pass-noPassPurchased">
+                                    <div>
+                                        You have not purchased any pass.{" "}<Link to="/buy-pass">Buy Now!</Link>
+                                    </div>
+                                </div>
+                            )
+                        }
+                        <img id="dashboard-pass-image" src={passBg} alt="ticket" style={{
+                            filter: `sepia(${(userData && userData.pass_type) ? 0 : 0.9}) blur(${(userData && userData.pass_type) ? 0 : 2}px)`
+                        }}>
+                        </img>
+                        {/* without container */}
+                        {/* {
+								userData && userData.pass_type !== 0 && (
+									<div className="dashboard-pass-registrationID">
+										Registration ID: {userData.registration_id}
+									</div>
+								)
+						} */}
+
+
+                        {/* <div
                             id="dashboard-pass-container"
                             onClick={() => handleDownload(userData.pass_type)}
 							style={{
@@ -386,9 +434,66 @@ function Dashboard() {
 									</div>
 								)
 							}
-						</div>
-						
-						<div id="dashboard-download-ticket-wrapper">
+						</div> */}
+                        <div className="dashboard-pass-left-side" style={{
+                            filter: `sepia(${(userData && userData.pass_type) ? 0 : 0.9}) blur(${(userData && userData.pass_type) ? 0 : 2}px)`
+                        }}>
+                            {/* <img id="dashboard-logo-img" src={PrometeoLogo} alt="Logo"></img> */}
+                            {
+                                userData && userData.pass_type !== 0 && (
+                                    <div className="dashboard-pass-registrationID">
+                                        <div className="registration-text">Registration ID: </div>{userData.registration_id}
+                                    </div>
+                                )
+                            }
+                            {/* {
+                                user.username[0] === 'P' ? user.username = "Test " + user.username : null
+                            } */}
+                            <div className="pass-details">
+                                <div className="middle-text" style={{
+                                    fontSize: window.innerWidth > 800 ? `calc((11 / ${maxNameLen}) * 0.022 * 0.67 * 92vw)` : `calc((11 / ${maxNameLen}) * 0.022 * 92vw)`
+                                }}>{user.username.replace("_", " ")}</div>
+                                <div className="middle-text-mail" style={{
+                                    fontSize: window.innerWidth > 800 ? `calc((10 / ${emailLength}) * 0.02 * 0.67 * 92vw)` : `calc((10 / ${emailLength}) * 0.02 * 92vw)`
+                                }}>{user.email.replace('@', ' @')}</div>
+                            </div>
+                        </div>
+                        {/* <div id="dashboard-pass-left-side"></div> */}
+                        {/* linear-gradient(90deg, rgb(175 197 8 / 70%) 0%, rgb(200 128 14 / 70%) 65%, rgb(204 59 12 / 70%) 100%) center center / cover no-repeat,  */}
+                        {/* <div id="dashboard-pass-right-side" style={{
+                            background: "rgb(226,31,181)",
+                            background: `linear-gradient(90deg, rgba(175 197 8 / 70%) 0%, rgba(200 128 14 / 70%) 65%, rgba(204 59 12 / 70%) 100%), url(https://i.pinimg.com/564x/cb/e4/2c/cbe42c0f273e6cd3acfd1de180ba5224.jpg)`,
+                            background: `linear-gradient(90deg, rgba(118,133,0,0.7) 0%, rgba(121,78,9,0.7) 65%, rgba(129,37,7,0.7) 100%), url(https://i.pinimg.com/564x/cb/e4/2c/cbe42c0f273e6cd3acfd1de180ba5224.jpg)`,
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                            backgroundRepeat: "no-repeat",
+                            }}>
+                            <h3 id="prometeo-date">20th-23rd Jan 2023</h3>
+                            <div id="pass-middle-details">
+                                {
+                                    userData && userData.pass_type !== 0 && (
+                                        <h4 className="middle-text">{user.username.replace("_", " ")}</h4>
+                                    )
+                                }
+                                {
+                                    userData && userData.pass_type !== 0 && (
+                                        <h4 className="middle-text">{user.email}</h4>
+                                    )
+                                }
+                                
+                                
+                            </div> */}
+                        {/* <div id="pass-bottom-right">
+                            <div id="location-container">
+                                <h3 id="prometeo-location"><img id="dashboard-map" src={Gmap} alt="map"/> IIT Jodhpur</h3>
+                            </div>
+                            <h6 id="prometeo-valid">VALID TILL 23rd Jan 11:59 PM</h6>
+                            </div> */}
+
+                        {/* </div> */}
+
+
+                        {/* <div id="dashboard-download-ticket-wrapper">
 							<div 
 								id="dashboard-download-ticket"
 								style={{
@@ -405,7 +510,37 @@ function Dashboard() {
 									)
 								}
 							</div>
-						</div>
+						</div> */}
+                    </div>
+                </div>
+                <div id="dashboard-download-ticket-wrapper">
+                    <div 
+                        id="dashboard-download-ticket"
+                        style={{
+                            backgroundImage: `url(${passBg})`,
+                        }}
+                    >
+                        <div className="dashboard-pass-left-side">
+                            {
+                                userData && userData.pass_type !== 0 && (
+                                    <div className="dashboard-pass-registrationID" style={{
+                                        fontSize: "calc(0.025 * 1771px)",
+                                    }}>
+                                        <div className="registration-text" style={{
+                                            fontSize: "calc(0.014 * 1771px)"
+                                        }}>Registration ID: </div>{userData.registration_id}
+                                    </div>
+                                )
+                            }
+                            <div className="pass-details">
+                                <div className="middle-text" style={{
+                                    fontSize: `calc((11 / ${maxNameLen}) * 0.022 * 1771px)`,
+                                }}>{user.username.replace("_", " ")}</div>
+                                <div className="middle-text-mail" style={{
+                                    fontSize: `calc((10 / ${emailLength}) * 0.02 * 1771px)`,
+                                }}>{user.email.replace('@', ' @')}</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div id="dashboard-registeredEvents">
@@ -434,9 +569,9 @@ function Dashboard() {
                                         </div>
                                         <div className="dashboard-registeredEvents-content-event-details">
                                             {key && (
-												<div className="dashboard-registeredEvents-content-event-title">
-													{key}
-												</div>
+                                                <div className="dashboard-registeredEvents-content-event-title">
+                                                    {key}
+                                                </div>
                                             )}
                                             {registeredEvents[key].date && (
                                                 <div className="dashboard-registeredEvents-content-event-date">
